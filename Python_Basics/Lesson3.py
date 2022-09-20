e5c545fcb69df13402852733b4583afca5b36702
Pierre Swtich <a.stolarski.as@gmail.com>
2022-09-20 21:04:04 +0200

    Completing the file lesson3 for Python_Basics

---
+++ Python_Basics/Lesson3.py
@@ -0,0 +1,58 @@
+# Logic operators
+
+#addition
+sum = 1 + 2
+
+print(sum)
+
+#Substracting
+
+substrtacting = 2 - 1
+
+print(substrtacting)
+
+#Bravely
+
+rest = 11 % 3
+
+print(rest)
+
+# condemnation
+
+condemnation = 10 ** 3
+
+print(condemnation)
+
+# multiplications for strings
+
+multistring = ' Said String |' * 10
+
+print(multistring)
+
+# even and odd  umbers
+
+even = [2,4,6,8]
+
+odd = [1,3,5,7]
+
+neutral = even + odd
+
+print(neutral)
+
+# not equal
+
+firstNumber = 1
+
+secondNumber = 2
+
+print('')
+
+print('This is firstnumber: ' + str(firstNumber))
+print('This is secondnumber: ' + str(firstNumber))
+
+print('')
+
+print('H: F is bigger than S: '+ str(firstNumber>secondNumber))
+print('H: S is bigger than F: '+ str(firstNumber<secondNumber))
+print('H: S is equal to F: '+ str(firstNumber==secondNumber))
+print('H: S is NOT equal to F: '+ str(firstNumber!=secondNumber))

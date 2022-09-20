289cdd8c3a53b6866e8150a9931654ed11365713
Pierre Swtich <a.stolarski.as@gmail.com>
2022-09-20 20:05:27 +0200

    Implementing the Script generating random numbers for Lesson 1

--- Python_Basics/Lesson1.py
+++ Python_Basics/Lesson1.py
@@ -1,1 +1,9 @@
 print("Hello World")
+
+from email.generator import Generator
+import random
+
+#Random number Generator
+
+number = random.randint(1,10)
+print('Randomly drawn number is: ', number)

2a6bc9b1bde82808924cf87826af6eae02d4ffd5
Pierre Swtich <a.stolarski.as@gmail.com>
2022-09-20 20:23:34 +0200

    Update the 2nd lesson with data from next lesson

--- Python_Basics/Lesson2.py
+++ Python_Basics/Lesson2.py
@@ -1,9 +1,26 @@
-print("Hello World")
+#VARIABLES
 
-from email.generator import Generator
-import random
+basicName = 'Ala ma kota'
 
-#Random number Generator
+# True | False
 
-number = random.randint(1,10)
-print('Randomly drawn number is: ', number)
+print(5==0)
+print(5==5)
+
+number = 1
+
+number1 = 2
+
+#Check
+
+print(number + number1)
+print(number == number1)
+print(number != number1)
+
+#Lists
+
+list = [1,2]
+
+print(list)
+print('Print first element on the list: ' + list[0])
+print('Print first element on the list: ' + list[1])

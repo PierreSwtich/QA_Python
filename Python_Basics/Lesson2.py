4ade209becb61f32ab0ad02a79c8acdbba9e99af
Pierre Swtich <a.stolarski.as@gmail.com>
2022-09-20 20:38:48 +0200

    Finishing the lessons 2, and updating the file

--- Python_Basics/Lesson2.py
+++ Python_Basics/Lesson2.py
@@ -22,5 +22,37 @@ print(number != number1)
 list = [1,2]
 
 print(list)
-print('Print first element on the list: ' + list[0])
-print('Print first element on the list: ' + list[1])
+print('First item on the list:')
+print(list[0])
+print('Second item on the list:')
+print(list[1])
+
+# List with numbers and string
+
+list2 = ['Ala','ma','kota',9]
+
+print(list2[0:3]) #Print list elments only strings
+
+# Testing variables
+
+text = 'test'
+text1 = 'Hello, my name is "Pierre"'
+print('')
+print(text1)
+
+#Next chapter different pyhton :D 
+
+cities = {'city':['Warszawa','Gdansk', 'Bratyslawa']}
+
+print(cities['city']) #Print all the cities in the list
+
+print('')
+
+print(cities['city'][0]) # It returns only the first city from the list
+
+
+
+
+
+
+
